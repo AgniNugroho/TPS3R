a097355b04686c7036d051f1a285fef9c15b6519
AgniNugroho <agniwwe@gmail.com>
2026-09-07 23:28:07 +0700

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ["10.43.87.207", "10.164.110.207", "192.168.100.204"],
+    allowedDevOrigins: [
+        "10.43.87.207",
+        "10.164.110.207",
+        "192.168.100.204",
+        "192.168.18.25",
+    ],
 };
 
 export default nextConfig;
